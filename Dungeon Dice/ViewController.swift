e26b8fb782aa27cb1c18d25a7f927eb104c2c63c
Nick Merc <nmercadante@zagmail.gonzaga.edu>
2020-02-11 10:40:11 -0800

    added randomization code

--- Dungeon Dice/ViewController.swift
+++ Dungeon Dice/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelText.text = " "
         // Do any additional setup after loading the view.
+       
     }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...(sender as AnyObject).tag)
+        labelText.text = "You Rolled a \(diceRoll) on a \(sender.tag)-sided dice"
+        
 
+    }
+    //number
+    
+    //generate a random number
+    //change the label to have a random number
 
 }
 
